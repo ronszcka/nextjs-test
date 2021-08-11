c0ce582f6fd45680416f27b2ff3f568951626c3c
Adriano Ronszcka <ronszcka@MBP-de-Adriano.lan>
2021-08-11 10:13:27 -0300

    Ninjas fetch and styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 export default function Home() {
     return (
-      <div>
-          <h1>about...</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos quaerat aut, odit molestiae at debitis consequatur. Placeat commodi, eveniet quos, doloremque deleniti tempore iure voluptates in facere sit debitis dolore?</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos quaerat aut, odit molestiae at debitis consequatur. Placeat commodi, eveniet quos, doloremque deleniti tempore iure voluptates in facere sit debitis dolore?</p>
-      </div>
+      <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+        <div>
+            <h1>about...</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos quaerat aut, odit molestiae at debitis consequatur. Placeat commodi, eveniet quos, doloremque deleniti tempore iure voluptates in facere sit debitis dolore?</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quos quaerat aut, odit molestiae at debitis consequatur. Placeat commodi, eveniet quos, doloremque deleniti tempore iure voluptates in facere sit debitis dolore?</p>
+        </div>
+      </>
     )
   }
   
--- src/pages/ninjas/index.tsx
+++ src/pages/ninjas/index.tsx
@@ -1,11 +1,39 @@
-export default function Home() {
-    return (
-      <div>
-          <h1>ninjas...</h1>
-          <p>A</p>
-          <p>B</p>
-          <p>C</p>
-      </div>
-    )
+import Head from "next/head";
+
+import styles from '../../../styles/Ninjas.module.css';
+
+export const getStaticProps = async () => {
+
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+
+  const data = await res.json();
+
+  return {
+    props: {
+      ninjas: data
+    }
   }
+
+}
+
+export default function Ninjas({ ninjas }) {
+  return (
+    <>
+    <Head>
+      <title>Ninja List | List</title>
+      <meta name="keywords" content="ninjas" />
+    </Head>
+      <div>
+          <h1>All ninjas...</h1>
+          { ninjas.map(ninja => (
+            <div key={ninja.id}>
+              <a className={styles.single}>
+                <h3>{ ninja.name }</h3>
+              </a>
+            </div>
+          ))}
+      </div>
+    </>
+  )
+}
   
